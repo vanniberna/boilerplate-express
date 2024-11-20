fc848fd7ff09a54a52d0455b840faa096c307b5e
Giorgio Chiappa <giorgio.chiappa@posteo.net>
2024-11-20 09:55:41 +0100

    Made myApp.js send a HTML file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let app = express();
+
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+}); 
+
+/*
 app.get('/', function(req, res) {
     res.send('Hello Express');
-  });
+  }); */
 
 
 
