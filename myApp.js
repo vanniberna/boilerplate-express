7917a2d25d80d38d46c9fbc63fd0574b52482abb
Giorgio Chiappa <giorgio.chiappa@posteo.net>
2024-11-20 10:42:08 +0100

    Add json send to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
 let express = require('express');
 let app = express();
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
+
+
+
+/*
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 }); 
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); */
 
 /*
 app.get('/', function(req, res) {
