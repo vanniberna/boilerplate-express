4f280ff4b15a18df02f4f0ebb25424061fe50466
Giorgio Chiappa <giorgio.chiappa@posteo.net>
2024-11-20 10:27:03 +0100

    Serve assets to myApp.js

--- myApp.js
+++ myApp.js
@@ -6,6 +6,8 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 }); 
 
+app.use("/public", express.static(__dirname + "/public"));
+
 /*
 app.get('/', function(req, res) {
     res.send('Hello Express');
